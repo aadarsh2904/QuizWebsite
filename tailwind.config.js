70cd5de89ef5b955e6b6b0a24036c27e4b13459c
aadarsh2904 <aadarshranjan2904@gmail.com>
2024-05-29 20:51:14 +0530

    Basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      greyishh:"#0d1616"
+    },
+  },
+  plugins: [],
+}
+
