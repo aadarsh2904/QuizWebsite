976c154d80c100bc8fe03a75d90495c634ad7758
Apoorv <apoorvtandon814@gmail.com>
2024-05-30 12:04:37 +0530

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      greyishh:"#0d1616"
+    extend: {
+      fontFamily:{
+        joti:['joti']
+
+      }
     },
   },
   plugins: [],
 }
-
